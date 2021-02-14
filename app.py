111992a7572593fb274a8d21751658542254773f
dogukankeskinoglu <dogukan.keskinoglu.ege@gmail.com>
2021-02-15 00:47:29 +0300

    add question database

--- app.py
+++ app.py
@@ -78,13 +78,13 @@ def show_exams():
         examdetails = json.loads(request.data)
         exam_id=manage.getExam(exam_object.exam_name)
         for i in examdetails:
-            question=str(i["value"]["question"])
-            a_choice=str(i["value"]["a_choice"])
-            b_choice=str(i["value"]["b_choice"])
-            c_choice=str(i["value"]["c_choice"])
-            d_choice=str(i["value"]["d_choice"])
-            e_choice=str(i["value"]["e_choice"])
-            true_answer_choice=str(i["value"]["true_answer_choice"])
+            question=i["value"]["question"]
+            a_choice=i["value"]["a_choice"]
+            b_choice=i["value"]["b_choice"]
+            c_choice=i["value"]["c_choice"]
+            d_choice=i["value"]["d_choice"]
+            e_choice=i["value"]["e_choice"]
+            true_answer_choice=i["value"]["true_answer_choice"]
             question_point=int(i["value"]["question_point"])
             all_choice=a_choice+"*_*"+b_choice+"*_*"+c_choice+"*_*"+e_choice+"*_*"+e_choice
             question_object=Question(exam_id,question,all_choices,true_answer_choice,question_point)
