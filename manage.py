9c002fdad8dc26c0f5b40f577138aecc24439e95
dogukankeskinoglu <dogukan.keskinoglu.ege@gmail.com>
2021-02-15 17:23:37 +0300

    add question database and show it with question object

--- manage.py
+++ manage.py
@@ -72,11 +72,11 @@ def insertExamDataBase(exam:Exam):
         cursor.execute("INSERT INTO Sinav(sinav_adi,sinav_baslama_tarihi,sinav_bitis_tarihi) VALUES (%s, %s,%s);",(exam.exam_name,exam.exam_baslama_tarihi,exam.exam_bitis_tarihi))     
     db.commit()
 
-def insertQuestionDataBase(sinav_id,soru_icerik,soru_siklari,dogru_cevap,puan):
+def insertQuestionDataBase(q:Question):
     #Soru(Soru_id,soru_sınav_id,soru_metni,soru_siklari,soru_dogru_cevap,soru_puani)
     db=Database()
     with db.get_cursor() as cursor:
-        cursor.execute("INSERT INTO Soru(soru_sinav_id,soru_metni,soru_siklari,soru_dogru_cevap,soru_puani) VALUES (%s, %s,%s,%s,%s);",(sinav_id,soru_icerik,soru_siklari,dogru_cevap,puan))
+        cursor.execute("INSERT INTO Soru(soru_sinav_id,soru_metni,soru_siklari,soru_dogru_cevap,soru_puani) VALUES (%s, %s,%s,%s,%s);",(q.question_exam_id,q.question_content,q.question_choices,q.correct_answer,q.question_point))
     db.commit()
 
 def getExam(examname):
