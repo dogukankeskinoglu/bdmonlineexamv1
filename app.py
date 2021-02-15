190b1035e0289994ac43e50875d48f8df7182861
dogukankeskinoglu <dogukan.keskinoglu.ege@gmail.com>
2021-02-15 13:59:20 +0300

    add question database v2

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def show_exams():
             manage.insertQuestionDataBase(question_object)"""
         all_choice="a_choice"+"*_*"+"b_choice"+"*_*"+"c_choice"+"*_*"+"d_choice"+"*_*"+"e_choice"
         question_object=Question(3,"question1",all_choice,"b",5)
-        #manage.insertQuestionDataBase(question_object)
+        manage.insertQuestionDataBase(question_object)
         #print(examdetails,sys.stdout.flush())
     # Sınav(sınav_id,sinav_adi,sınav_baslama,sınav_bitis)
         
