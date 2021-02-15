ba3a2f5ed71bd266252915b3f49315545b47afc7
dogukankeskinoglu <dogukan.keskinoglu.ege@gmail.com>
2021-02-15 17:48:44 +0300

    add question database and rotate

--- templates/exam.py
+++ templates/exam.py
@@ -4,9 +4,9 @@ class Exam:
     exam_baslama_tarihi=""
     exam_bitis_tarihi=""
     exam_count=0
-    def __init__(self, e_id, e_name,e_baslama_tarihi,e_bitis_tarihi): 
+    def __init__(self,e_name,e_baslama_tarihi,e_bitis_tarihi): 
         Exam.exam_count=Exam.exam_count+1
-        self.exam_id=e_id
+        self.exam_id=Exam.exam_count
         self.exam_name=e_name
         self.exam_baslama_tarihi=e_baslama_tarihi
         self.exam_bitis_tarihi=e_bitis_tarihi
