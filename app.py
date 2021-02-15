1b6db5b9f33ce6aa0dee89388620fba078e70cd0
dogukankeskinoglu <dogukan.keskinoglu.ege@gmail.com>
2021-02-15 14:19:02 +0300

    add question database v5

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def show_exams():
         """
         #examdetails = json.loads(request.data)
         exam_id=manage.getExam(exam_object.exam_name)
-        """for i in examdetails:
+        for i in examdetails:
             question=i["value"]["question"]
             a_choice=i["value"]["a_choice"]
             b_choice=i["value"]["b_choice"]
@@ -87,11 +87,11 @@ def show_exams():
             true_answer_choice=i["value"]["true_answer_choice"]
             question_point=int(i["value"]["question_point"])
             all_choice=a_choice+"*_*"+b_choice+"*_*"+c_choice+"*_*"+e_choice+"*_*"+e_choice
-            question_object=Question(exam_id,question,all_choices,true_answer_choice,question_point)
-            manage.insertQuestionDataBase(question_object)"""
+            #question_object=Question(exam_id,question,all_choices,true_answer_choice,question_point)
+            manage.insertQuestionDataBase(exam_id,question,all_choices,true_answer_choice,question_point)
         #all_choice="a_choice"+"*_*"+"b_choice"+"*_*"+"c_choice"+"*_*"+"d_choice"+"*_*"+"e_choice"
         #question_object=Question()
-        manage.insertQuestionDataBase(exam_id,"question1","ashshshhssh","b",5)
+        #manage.insertQuestionDataBase(exam_id,"question1","ashshshhssh","b",5)
         #print(examdetails,sys.stdout.flush())
     # Sınav(sınav_id,sinav_adi,sınav_baslama,sınav_bitis)
         
