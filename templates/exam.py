de80cd2338ad5a1f08e02a53de18cf46e95aa545
dogukankeskinoglu <dogukan.keskinoglu.ege@gmail.com>
2021-02-15 17:57:33 +0300

    add question database v2

--- templates/exam.py
+++ templates/exam.py
@@ -3,10 +3,8 @@ class Exam:
     exam_name=""
     exam_baslama_tarihi=""
     exam_bitis_tarihi=""
-    exam_count=0
-    def __init__(self,e_name,e_baslama_tarihi,e_bitis_tarihi): 
-        Exam.exam_count=Exam.exam_count+1
-        self.exam_id=Exam.exam_count
+    def __init__(self, e_id, e_name,e_baslama_tarihi,e_bitis_tarihi): 
+        self.exam_id=e_id
         self.exam_name=e_name
         self.exam_baslama_tarihi=e_baslama_tarihi
         self.exam_bitis_tarihi=e_bitis_tarihi
