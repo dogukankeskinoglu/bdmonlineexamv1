a951a8e1fdd49c6f102b35462b8d872cb84644ab
dogukankeskinoglu <dogukan.keskinoglu.ege@gmail.com>
2021-02-15 16:25:23 +0300

    examdetails basarili

--- app.py
+++ app.py
@@ -66,17 +66,17 @@ def show_exams():
         for i in examdetails["data"]:
             #sayac+=1
             question=i["value"]["question"]
-            """a_choice=i["value"]["a_choice"]
+            a_choice=i["value"]["a_choice"]
             b_choice=i["value"]["b_choice"]
             c_choice=i["value"]["c_choice"]
             d_choice=i["value"]["d_choice"]
             e_choice=i["value"]["e_choice"]
             true_answer_choice=i["value"]["true_answer_choice"]
-            question_point=int(i["value"]["question_point"])"""
-            #all_choice=a_choice+"*_*"+b_choice+"*_*"+c_choice+"*_*"+e_choice+"*_*"+e_choice
+            question_point=int(i["value"]["question_point"])
+            all_choice=a_choice+"*_*"+b_choice+"*_*"+c_choice+"*_*"+e_choice+"*_*"+e_choice
             #question_object=Question(exam_id,question,all_choice,true_answer_choice,question_point)
             #manage.insertQuestionDataBase(question_object)
-            manage.insertQuestionDataBase(exam_id,"question1","ashshshhssh","b",5)
+            manage.insertQuestionDataBase(exam_id,question,all_choice,true_answer_choice,question_point)
         #all_choice="a_choice"+"*_*"+"b_choice"+"*_*"+"c_choice"+"*_*"+"d_choice"+"*_*"+"e_choice"
         #question_object=Question(3,"question1",all_choice,"b",5)
         #manage.insertQuestionDataBase(question_object)
