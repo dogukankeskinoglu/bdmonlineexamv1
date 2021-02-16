73a354e60bca5c044ab656deccc3aed45b1535a8
dogukankeskinoglu <dogukan.keskinoglu.ege@gmail.com>
2021-02-16 20:50:59 +0300

    manage.py degisti

--- manage.py
+++ manage.py
@@ -179,5 +179,5 @@ elif args.command=="ss":
     db=Database()
     with db.get_cursor() as cursor:
         cursor.execute("SELECT (soru_sinav_id) FROM Soru WHERE soru_id= %s",(8,))
-        row=cursor.fetchone()
-        print(row)
+        row=cursor.fetchall()
+        print(row[0])
